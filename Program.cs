e22fb9a269361ac2213fcf116cfe096853108514
Jukebox <jukebox1412@gmail.com>
2023-02-12 22:08:45 -0700

    Added project

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Heartbeat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to exit!");
+            Console.ReadLine();
+        }
+    }
+}
